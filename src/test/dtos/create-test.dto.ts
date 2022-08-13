71d33a488e1d23077fbaca789a3163a54a199963
dryadsoft <dryadsoft@gmail.com>
2022-08-13 11:58:56 +0900

    class-validator 설정 - 2

--- src/test/dtos/create-test.dto.ts
+++ src/test/dtos/create-test.dto.ts
@@ -1,4 +1,5 @@
 import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, isBoolean, IsString, Length } from 'class-validator';
 
 @InputType()
 export class CreateTestDto {
@@ -15,11 +16,15 @@ export class CreateTestDto {
 @ArgsType()
 export class CreateTestDto2 {
   @Field((type) => String)
+  @IsString()
+  @Length(4)
   name: string;
+
   @Field((type) => Number)
   age: number;
   @Field((type) => String)
   address: string;
   @Field((type) => Boolean)
+  @IsBoolean()
   sex: boolean;
 }
