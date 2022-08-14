34128a25dccc7f2631845056db65d6e17e37301c
dryadsoft <dryadsoft@gmail.com>
2022-08-14 11:07:26 +0900

    users resolver, service 생성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,20 @@
-import { Field } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
 type RoleType = 'system' | 'admin' | 'client';
 @Entity()
+@ObjectType()
 export class User extends CoreEntity {
   @Column()
+  @Field((type) => String)
   email: string;
 
   @Column()
+  @Field((type) => String)
   password: string;
 
   @Column()
+  @Field((type) => String)
   role: RoleType;
 }
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver()
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query((returns) => [User])
+  users(): Promise<User[]> {
+    return this.usersService.getAll();
+  }
+}
