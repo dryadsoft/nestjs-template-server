130e6bb20763a5fe21670881622297a5a1f286c7
dryadsoft <dryadsoft@gmail.com>
2022-08-14 11:24:56 +0900

    Enum 타입 설정

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,19 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { IsEmail, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
-type RoleType = 'system' | 'admin' | 'client';
+enum UserRole {
+  System,
+  Admin,
+  Client,
+}
+registerEnumType(UserRole, { name: 'UserRole' });
 @InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
@@ -18,8 +28,8 @@ export class User extends CoreEntity {
   @IsString()
   password: string;
 
-  @Column()
-  @Field((type) => String)
+  @Column({ type: 'enum', enum: UserRole })
+  @Field((type) => UserRole)
   @IsString()
-  role: RoleType;
+  role: UserRole;
 }
