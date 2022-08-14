d3e8935fcfbd1a82c512afe2c975242aee362671
dryadsoft <dryadsoft@gmail.com>
2022-08-14 11:19:46 +0900

    users create-account생성

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,20 +1,25 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsEmail, IsString } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity } from 'typeorm';
 
 type RoleType = 'system' | 'admin' | 'client';
-@Entity()
+@InputType({ isAbstract: true })
 @ObjectType()
+@Entity()
 export class User extends CoreEntity {
   @Column()
   @Field((type) => String)
+  @IsEmail()
   email: string;
 
   @Column()
   @Field((type) => String)
+  @IsString()
   password: string;
 
   @Column()
   @Field((type) => String)
+  @IsString()
   role: RoleType;
 }
