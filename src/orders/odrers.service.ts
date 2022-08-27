2730ec3be5bd5ac25d627e132fd2c9a98fdb60a6
dryadsoft <dryadsoft@gmail.com>
2022-08-27 11:34:57 +0900

    create order mutation

--- src/orders/odrers.service.ts
+++ src/orders/odrers.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -8,4 +10,20 @@ export class OrderService {
   constructor(
     @InjectRepository(Order) private readonly orders: Repository<Order>,
   ) {}
+
+  async createOrder(
+    customer: User,
+    createOrderInput: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    try {
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not order',
+      };
+    }
+  }
 }
